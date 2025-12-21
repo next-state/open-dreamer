72c8847c442e2b639a0b5900d571e2ef483f0b0c
diego <martidiegom@gmail.com>
2025-12-21 22:54:16 +0000

    correct prefill noise

--- dreamer/generation.py
+++ dreamer/generation.py
@@ -25,6 +25,7 @@ class DenoiseSchedule:
         tau_indices: indices of the signal levels used during the denoising τ_idx = [0, k, 2k, ..., k_max].
         tau_idx_ctx: we pass slightly noised context frames, the index of that noise level (0.1 in the paper) is tau_idx_ctx.
         step_idx_ctx: the index of the noise level that starting from tau_idx_ctx brings you to 1.
+        tau_ctx: noise level of context frames during autoregressive rollout.
     """
 
     num_steps: int
@@ -35,6 +36,7 @@ class DenoiseSchedule:
     tau_indices: jax.Array
     step_idx_ctx: int
     tau_idx_ctx: int
+    tau_ctx: float
 
     @classmethod
     def init(cls, num_steps: int, k_max: int = 256, tau_ctx=0.9) -> "DenoiseSchedule":
@@ -57,7 +59,7 @@ class DenoiseSchedule:
         step_idx_ctx = int(jnp.round(-math.log2(1 - tau_ctx)))
         tau_idx_ctx = k_max - k_max // 2**step_idx_ctx
         
-        return cls(num_steps, k_max, d, step_idx, tau_values, tau_indices, step_idx_ctx, tau_idx_ctx)
+        return cls(num_steps, k_max, d, step_idx, tau_values, tau_indices, step_idx_ctx, tau_idx_ctx, tau_ctx)
     
 # ---------------------------
 # Single-step τ-ladder denoiser
@@ -69,6 +71,7 @@ def next_latent(
     schedule: DenoiseSchedule,
     action: jax.Array,                 # (B, 1)
     latent_shape: Tuple,                   # (B, 1, n_spatial, D_s)
+    prefill_length: int,
     rng: jax.Array,
     agent_tokens: jax.Array | None = None,  # (B, T_ctx+1, n_agent, d_model)
     caches: KVCache | None = None,
@@ -84,6 +87,7 @@ def next_latent(
         schedule: Precomputed DenoiseSchedule
         action: (B, 1) action for current step
         latent_shape: Tuple representing the shape of the latent (B, 1, n_spatial, D_s)
+        prefill_length: Number of ground truth latents passed during prefill.
         rng: Random number generator key
         agent_tokens: Optional agent tokens (B, T_ctx+1, n_agent, d_model)
         caches: KV cache for context frames (from previous finalized frames)
@@ -92,16 +96,23 @@ def next_latent(
 
     Returns:
         Tuple containing:
-        - latent_t_new: The denoised latent (B, 1, n_spatial, D_s)
+        - latent_t_final: The denoised latent (B, 1, n_spatial, D_s)
         - h_last: The final hidden state from dynamics (B, n_agent, d_model)
-        - caches_last: The updated KV cache
+        - caches_new: The updated KV cache
     """
-    rng, rng_latent = jax.random.split(rng, 2)
+    rng, rng_latent, rng_ctx = jax.random.split(rng, 3)
     noisy_latent = jax.random.normal(rng_latent, latent_shape)
     B = latent_shape[0]
 
-    action = action[:, None] if action.ndim == 1 else action
-    
+    noise_ctx = None
+    latents_ctx_noised = None
+    if latents_ctx is not None:
+        noise_prefill= jnp.zeros(latents_ctx[:, :prefill_length].shape)
+        noise_decode = jax.random.normal(rng_ctx, latents_ctx[:, prefill_length:].shape)
+        noise_ctx    = jnp.concatenate([noise_prefill, noise_decode], axis=1)
+        latents_ctx_noised = latents_ctx + (1 - schedule.tau_ctx) * noise_ctx
+
+    action = action[:, None] if action.ndim == 1 else action 
     
     def refinement_step(latent_t, s):
         tau_prev, tau_curr = schedule.tau_values[s], schedule.tau_values[s+1]
@@ -109,8 +120,6 @@ def next_latent(
         
         step_idx = schedule.step_idx
         tau_idx_val = schedule.tau_indices[s] 
-
-        step_idx_ctx, tau_idx_ctx = schedule.step_idx_ctx, schedule.tau_idx_ctx
 
         if caches is not None:
             latent_input, actions_input = latent_t, action
@@ -121,21 +130,23 @@ def next_latent(
             assert agent_tokens is None or agent_tokens.shape[1] == noisy_latent.shape[1] 
         
         else: # Used only for debugging.
-            assert latents_ctx is not None and actions_ctx is not None
-            latent_input  = jnp.concatenate([latents_ctx, latent_t], axis=1)  # (B, T_ctx+1, n_spatial, D_s)
+            assert latents_ctx_noised is not None and actions_ctx is not None
+            latent_input  = jnp.concatenate([latents_ctx_noised, latent_t], axis=1)  # (B, T_ctx+1, n_spatial, D_s)
             actions_input = jnp.concatenate([actions_ctx, action],   axis=1)  # (B, T_ctx+1)
 
-            T_ctx = latent_input.shape[1]
-            step_idx_ctx  = jnp.full((B, T_ctx), step_idx_ctx, dtype=jnp.int32)  # FIXME: should be schedule.step_idx for gt ctx
-            step_idx_curr = jnp.full((B, 1),     step_idx,     dtype=jnp.int32)
-            step_idx = jnp.concatenate([step_idx_ctx, step_idx_curr], axis=1)
+            decode_length = latents_ctx_noised.shape[1] - prefill_length
+            step_idx_prefill= jnp.full((B, prefill_length), schedule.step_idx, dtype=jnp.int32)
+            step_idx_decode = jnp.full((B, decode_length),  schedule.step_idx_ctx, dtype=jnp.int32)
+            step_idx_curr   = jnp.full((B, 1), step_idx, dtype=jnp.int32)
+            step_idx = jnp.concatenate([step_idx_prefill, step_idx_decode, step_idx_curr], axis=1)
             
-            tau_idx_ctx  = jnp.full((B, T_ctx), tau_idx_ctx, dtype=jnp.int32)  # FIXME: should be schedule.k_max for gt ctx
-            tau_idx_curr = jnp.full((B, 1),     tau_idx_val, dtype=jnp.int32)
-            tau_idx = jnp.concatenate([tau_idx_ctx, tau_idx_curr], axis=1) # (B, T_ctx+1)
+            tau_idx_prefill= jnp.full((B, prefill_length), schedule.k_max, dtype=jnp.int32)
+            tau_idx_decode = jnp.full((B, decode_length), schedule.tau_idx_ctx, dtype=jnp.int32)
+            tau_idx_curr   = jnp.full((B, 1), tau_idx_val, dtype=jnp.int32)
+            tau_idx = jnp.concatenate([tau_idx_prefill, tau_idx_decode, tau_idx_curr], axis=1) # (B, T_ctx+1)
 
         # Dynamics call
-        latent_clean_pred_seq, (h_seq, caches_updated) = dynamics.apply(dyn_vars, actions_input, step_idx, tau_idx, latent_input, agent_tokens=agent_tokens, deterministic=True, caches=caches)
+        latent_clean_pred_seq, (h_seq, _) = dynamics.apply(dyn_vars, actions_input, step_idx, tau_idx, latent_input, agent_tokens=agent_tokens, deterministic=True, caches=caches)
 
         latent_clean_pred = latent_clean_pred_seq[:, -1:, :, :]  # (B, 1, n_spatial, D_s)
         h_last = h_seq[:, -1, :, :] if isinstance(h_seq, jax.Array) else h_seq # (B, n_agent, d_model)
@@ -143,20 +154,29 @@ def next_latent(
         # Per-step mixing toward clean latent
         latent_t_new = (1.0 - alpha) * latent_t + alpha * latent_clean_pred
 
-        return latent_t_new, (h_last, caches_updated)
+        return latent_t_new, h_last
 
     # Run τ-ladder with JAX control flow using scan to keep carry/output structure consistent.
-    z_t_final, (h_history, caches_history) = jax.lax.scan(
+    latent_t_final, h_history = jax.lax.scan(
         refinement_step,
         noisy_latent,
         jnp.arange(schedule.num_steps),
     )
+    
+    if caches is not None:
+        # update caches by doing one more refinement step with tau_ctx
+        step_idx= jnp.full((B, 1), schedule.step_idx_ctx, dtype=jnp.int32)
+        tau_idx = jnp.full((B, 1), schedule.tau_idx_ctx,  dtype=jnp.int32)
 
-    h_last = h_history[-1] if h_history is not None else None  # (B, n_agent, d_model)
+        _, (h_seq_final, caches_new) = dynamics.apply(dyn_vars, action, step_idx, tau_idx, latent_t_final, agent_tokens=agent_tokens, deterministic=True, caches=caches)
+        h_last = h_seq_final[:, -1, :, :] if isinstance(h_seq_final, jax.Array) else h_seq_final
+    else:
+        h_last = h_history[-1] if h_history is not None else None  # (B, n_agent, d_model)
+        caches_new = None
+
     assert isinstance(h_last, jax.Array) or h_last is None
-    caches_last = jax.tree.map(lambda x: x[-1], caches_history)
 
-    return z_t_final, h_last, caches_last, rng  
+    return latent_t_final, h_last, caches_new, rng  
 
 
 def latent_rollout(
@@ -199,7 +219,7 @@ def latent_rollout(
     caches = dynamics.create_static_caches(batch_size=B, n_spatial=n_spatial, window_size=window_size)
     
     # Run dynamics on context to prefill caches and get last hidden state
-    # Use clean signal for context 
+    # Use clean signal for ground truth context 
     step_idx_ctx= jnp.full((B, T_ctx), schedule.step_idx, dtype=jnp.int32)
     tau_idx_ctx = jnp.full((B, T_ctx), schedule.k_max, dtype=jnp.int32)
     
@@ -224,7 +244,7 @@ def latent_rollout(
             action = jax.random.categorical(rng_action, logits) # (B, L)
         
         # Predict next latent (denoising)
-        latent_next, h_next, caches_next, rng = next_latent(dynamics, dyn_vars, schedule, action, latent_shape, rng, caches=caches_t)
+        latent_next, h_next, caches_next, rng = next_latent(dynamics, dyn_vars, schedule, action, latent_shape, T_ctx, rng, caches=caches_t)
         
         return (h_next, caches_next, rng), latent_next[:,0] # latent_next is (B, 1, n_spatial, D_s) 
 
